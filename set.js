8ed733c8226647a83ad9d62aa87f7bf283b39634
Darkkenan <167613032+Darkkenan@users.noreply.github.com>
2024-08-01 10:59:39 +0200

    set.js

--- set.js
+++ set.js
@@ -6,11 +6,11 @@ const path = require("path");
 const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
-    : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
-    PREFIXE: process.env.PREFIX || "+",
-    OWNER_NAME: process.env.OWNER_NAME || "Keith Keizzah",
-    NUMERO_OWNER : process.env.OWNER_NUMBER || "",              
+    : process.env.DATABASE_URL;postgresql://hacking_database_q4zj_user:CuF96cXla7yH23fDrGsHFrKQa2e0Tog6@dpg-cqjo9ruehbks73cf84mg-a.oregon-postgres.render.com/hacking_database_q4zj
+module.exports = { session: process.env.SESSION_ID || 'eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTUFOaWJET0dvU0JrOWNGWFdnZlVlSkFtbk1HYjhrSXlIaXdtSGkxWS9VVT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMGhXenRvcHIxMzZOekhPQWw1OXB2cWdwNW10dkYybVViQ0RXQ0hWWFBoVT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJhSWZyS2l6K1lMaDY5WGJMUFNwVHhKUVJZVnlPQnRuUko4TDF2MVRQdDFZPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJiV3RMazArNk00aG1JWm03OUFMSG1ENFE1RGlEQW5nTVlKTVhXVktLV2dNPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjZHY0wvMHFxdzBnWTFmN082MmVRNkRJRzdWOFdhSEw2RUhEcElUNlJkVm89In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImFEM2tQUGJKNHdvU0RTaSsrcXd2UmJ3WjFFZmNUc1BpMW1VdGZERE5pMGs9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSUtxUC9IOW1NVDgzaUZLQ2NpdE5iYnlYaDM0emp4UzNpV3MxcE4xeHRWbz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiL0pmMHBheTRrSFBIWld2aml5TUF5dkVYaFp6Zms3K0M0c3F4UHppOUtGZz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImNYUm1PSzA5WERoeWUzTTQvcllzM0hpajdjMlNLdFlOckx1SkFSNjZIZVlTU1UreGdweENkdFRRNkUrdTZtV2UwalFIWlZ4VTdTT1BqVTRzMmI4bmp3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTAxLCJhZHZTZWNyZXRLZXkiOiJwTVRpbWRNdGdQOFMvYk1jeDhkejNzMy9DZ1BJaWk1aUJuN2grL3oxa25nPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJzdG44LXhlUlIyR0pKQWZoaVh5dkFBIiwicGhvbmVJZCI6ImQ2MmEzMGRlLWNmZGMtNGQ2Ny1hN2UyLTNhOTkyNGY4NDY0ZCIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ4QjVsZkppRy8vckd1OWZrc25sRlFlODVHaFk9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOXZPTDhUVGtSWnlCK1g1Vkp5b2FlQXVRbE9RPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IlpQQUFEWEZSIiwibWUiOnsiaWQiOiIyNDM5NzI2ODE5NzQ6MjVAcy53aGF0c2FwcC5uZXQiLCJuYW1lIjoiS8OLTsOETvOxoo8ifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0pyemc5SURFS3FQcmJVR0dBSWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6InJja2pCS21BSG5iMi84MXVPU21GWVN2UkZGSHJJMVFjbVVldTlJQU5obHM9IiwiYWNjb3VudFNpZ25hdHVyZSI6ImQ1dUNRY0ZWMzViWjJzUmsxYWQ0SHZ6MlByaEJvVGc0YzNrWllZZ1p4TGNvR3lPRVFPM2FEajhPWnBIQUNNcmphWXNpY1hOMWcvdE5YWGxMdXphekNRPT0iLCJkZXZpY2VTaWduYXR1cmUiOiI3VW5paTE5MVRHcWdRUXBDVThIWHovMHBCb3dBenFPSWpXZk0wczhGbUpaTmZXZ3BqSFNjb2l1Mkh0bG1QMnl4KytYRFFTeGVjb0VGRGNUb0ZaSmpndz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjI0Mzk3MjY4MTk3NDoyNUBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJhM0pJd1NwZ0I1Mjl2L05iamtwaFdFcjBSUlI2eU5VSEpsSHJ2U0FEWVpiIn19XSwicGxhdGZvcm0iOiJzbWJhIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIyNTAxMDQ4fQ==',
+    PREFIXE: process.env.PREFIX || ":",
+    OWNER_NAME: process.env.OWNER_NAME || "KËNÄN",
+    NUMERO_OWNER : process.env.OWNER_NUMBER || "243972681974",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
     BOT : process.env.BOT_NAME || 'HUNCHO MD',
@@ -27,7 +27,7 @@ module.exports = { session: process.env.SESSION_ID || '',
     ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
-        ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
+        ? "postgresql://hacking_database_q4zj_user:CuF96cXla7yH23fDrGsHFrKQa2e0Tog6@dpg-cqjo9ruehbks73cf84mg-a.oregon-postgres.render.com/hacking_database_q4zj",
     /* new Sequelize({
      dialect: 'sqlite',
      storage: DATABASE_URL,
